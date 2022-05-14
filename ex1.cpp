9bf6e149750756feb9dd52b3ab3828e855f1a34a
Massimo Belfiore <massimobelfiorechitarra@gmail.com>
2022-05-14 11:17:43 +0200

    chore: utilizzo del namespace std

--- ex1.cpp
+++ ex1.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
+using namespace std;
 
 int main(){
     int week;
 
     /* Input number from user */
-    std::cout << "Enter week number(1-7): " << std::endl;
-    std::cin >> week;
+    cout << "Enter week number(1-7): " << endl;
+    cin >> week;
 
     switch(week){
 
-        case 1: std::cout << "Monday" << std::endl;
+        case 1: cout << "Monday" << endl;
             break;
-        case 2: std::cout << "Tuesday" << std::endl;
+        case 2: cout << "Tuesday" << endl;
             break;       
-        case 3: std::cout << "Wednesday" << std::endl;
+        case 3: cout << "Wednesday" << endl;
             break;
-        case 4: std::cout << "Thursday" << std::endl;
+        case 4: cout << "Thursday" << endl;
             break;
-        case 5: std::cout << "Friday" << std::endl;
+        case 5: cout << "Friday" << endl;
             break;
-        case 6: std::cout << "Saturday" << std::endl;
+        case 6: cout << "Saturday" << endl;
             break;
-        case 7: std::cout << "Sunday" << std::endl;
+        case 7: cout << "Sunday" << endl;
             break;
 
         deafult:
-            std::cout << "Invalid input! Please enter week number between 1-7." << std::endl;
+            cout << "Invalid input! Please enter week number between 1-7." << endl;
     }
     
     return 0;
